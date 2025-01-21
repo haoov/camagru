cf90feccfacf818554fe0fc01a27dfa56ad46bc8
rsabbah <rpasabb@gmail.com>
2025-01-21 21:31:55 +0100

    proxy server and frontend server running database configured

---
+++ db/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+  mariadb-install-db --user=mysql --datadir=/var/lib/mysql --skip-test-db
+  service mariadb start
+  sleep 5
+
+  mysql << EOF
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWD}';
+CREATE DATABASE \`${DB_NAME}\`;
+GRANT ALL ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
+CREATE USER '${DB_ADMIN}'@'%' IDENTIFIED BY '${DB_ADMIN_PASSWD}';
+GRANT ALL ON \`${DB_NAME}\`.* TO '${DB_ADMIN}'@'%';
+FLUSH PRIVILEGES;
+EOF
+fi
+
+exec "$@"
